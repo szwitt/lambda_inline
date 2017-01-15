f2704048a07f27fa9ea3d5e8882ce3eb60ca6c10
szwitt <nospam@zwitt.net>
2017-01-15 13:50:28 -0800

    Updated to add variables
    Initial variables, will be moving to a dynamodb object in next version

--- iot_button_to_layer/publish_callback_to_layer.py
+++ iot_button_to_layer/publish_callback_to_layer.py
@@ -1,10 +1,10 @@
 import httplib, urllib
-import boto3
-import os
-import logging
 import json
-
+import logging
+import os
 from base64 import b64decode
+
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,7 +15,11 @@ app_uuid = boto3.client('kms').decrypt(CiphertextBlob=b64decode(eapp_uuid))['Pla
 ebearer_token = os.environ['bearer_token']
 bearer_token = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ebearer_token))['Plaintext']
 
-params = "{\n    \"sender_id\": \"layer:///identities/{identity}\",\n    \"parts\": [\n        {\n            \"body\": \"{requestor name} - Requested assistance thru Button :)\",\n            \"mime_type\": \"text/plain\"\n        }\n    ]\n}"
+user_id = 'fb4f632c-c7e5-466a-910b-a9c0a15f732f'
+user_name = 'Ben Hackett'
+conversation_uuid = 'a54be44e-6b8f-49f5-8d49-b82d0800060f'
+
+params = ("{\"sender_id\":\"layer:///identities/%s\",\"parts\":[{\"body\":\"%s - Requested assistance thru Button :)\",\"mime_type\":\"text/plain\"}]}" % (user_id, user_name))
 
 headers = {
     'accept': "application/vnd.layer+json; version=2.0",
@@ -28,7 +32,7 @@ def lambda_handler(event, context):
     logging.info('Received event: ' + json.dumps(event))
 
     conn = httplib.HTTPSConnection("api.layer.com")
-    conn.request("POST", "/apps/{app_id}/conversations/{conversation_id}/messages", params, headers)
+    conn.request("POST", "/apps/%s/conversations/%s/messages" % (app_uuid, conversation_uuid), params, headers)
     response = conn.getresponse()
 
     logger.info(response.status)
